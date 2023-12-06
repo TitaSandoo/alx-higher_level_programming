44703a01f35270a805036e48b11da52b73d7affb
TitaSandoo <fat.moh.ahmed@gmail.com>
2023-12-06 05:51:37 -0800

    alla

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = a_dictionary.copy()
-    for key in new_dictionary:
-        new_dictionary[key] *= 2
-    return(new_dictionary)
+    return {k: v * 2 for k, v in a_dictionary.items()}
